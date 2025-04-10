11427a6ca8823b45d3b9cef4d362e3a3d8a468b6
NAJEEBKHAN123 <najeebkhanlaku@gmail.com>
2025-04-10 22:41:43 +0500

    add everything

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,24 +1,103 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
 import axios from 'axios'
-import { response } from 'express'
+import toast from 'react-hot-toast'
 
 const BASE_URL = 'http://localhost:3000'
 
-export const useProductStore = create((set, get) =>({
-     products: [],
-     loading: false,
-     error: null,
+export const useProductStore = create((set, get) => ({
+    products: [],
+    loading: false,
+    error: null,
+    currentProduct: null,
 
-     fetchProducts: async () =>{
+
+ // form state
+  formData: {
+    name: "",
+    price: "",
+    image: "",
+  },
+
+  setFormData: (formData) => set({ formData }),
+  resetForm: () => set({ formData: { name: "", price: "", image: "" } }),
+
+  addProduct: async (e) => {
+    e.preventDefault();
+    set({ loading: true });
+
+    try {
+      const { formData } = get();
+      await axios.post(`${BASE_URL}/api/products`, formData);
+      await get().fetchProducts();
+      get().resetForm();
+      toast.success("Product added successfully");
+      document.getElementById("add_product_modal").close();
+    } catch (error) {
+      console.log("Error in addProduct function", error);
+      toast.error("Something went wrong");
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+    fetchProducts: async () => {
+        set({ loading: true })
+        try {
+            const res = await axios.get(`${BASE_URL}/api/products`)
+            set({ products: res.data.data, error: null })
+        } catch (err) {
+            if (err.response?.status === 429) {
+                set({ error: "Rate limit exceeded" })
+            } else {
+                set({ error: "Something went wrong" })
+            }
+        } finally {
+            set({ loading: false })
+        }
+    },
+
+    deleteProduct: async (id) =>{
         set({loading: true})
         try {
-            await axios.get(`${BASE_URL}/api/products`)
-            set({products: response.data.data, error: null})
-        } catch (err) {
-            if(err.status === 429) set({error: "Rate limit exceeded"})
-                else set({error: "Something went wrong"});
+            await axios.delete(`${BASE_URL}/api/products/${id}`)
+        } catch (error) {
+            console.log("error in deleting product", error)
+            toast.error("Someting went wrong")
         }finally{
             set({loading: false})
         }
-     }
+    },
+
+    fetchProduct: async(id) =>{
+       set({loading: true})
+
+       try {
+        const response = await axios.get(`${BASE_URL}/api/products/${id}`);
+        set({currentProduct: response.data.data,
+        formData: response.data.data,
+        error: null
+
+       })
+       } catch (error) {
+        console.log("Error in fetching Product", error)
+        set({error: "Something went wrong", currentProduct: null})
+       }finally{
+        set({loading: false})
+       }
+    },
+
+    updateProduct: async(id) =>{
+      set({loading: true})
+      try {
+        const {formData} = get()
+        const response = await axios.put(`${BASE_URL}/api/products/${id}`, formData);
+        set({currentProduct: response.data.data});
+        toast.success("Product updated successfully");
+      } catch (error) {
+        toast.err("Something went wrong")
+        console.log("Error in updated product fun", error)
+      }finally{
+        set({loading: false})
+      }
+    }
 }))
